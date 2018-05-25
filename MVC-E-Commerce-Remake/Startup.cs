1c3ee5695f7a2228b5a5a051a2c9fea2c875f81e
Aelux5216 <jason.stanford@students.plymouth.ac.uk>
2018-05-25 20:16:16 +0100

    Add project files.

---
+++ MVC-E-Commerce-Remake/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_E_Commerce_Remake.Startup))]
+namespace MVC_E_Commerce_Remake
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
